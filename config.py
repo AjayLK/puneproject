6b9e956a9360ef324896585150fe17df4c5f593c
Ajay <ajaykshirsagar500@gmail.com>
2022-10-28 23:32:05 +0530

    project configuration

---
+++ config.py
@@ -0,0 +1,11 @@
+
+import os
+
+
+CSV_FILE_PATH = "enter_path"
+
+PORT_NUMBER = 5005
+
+MODEL_FILE_PATH = os.path.join("predict_pune_model" , "pude_model.pkl")
+
+JSON_FILE_PATH = os.path.join("predict_pune_model" , "Pune_model_enconding_col.json")
